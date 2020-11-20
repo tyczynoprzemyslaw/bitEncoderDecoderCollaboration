3c846a3f0b88fafd875705eebcfcce5859eb76d5
tyczynoprzemyslaw <tyczyno.przemyslaw@gmail.com>
2020-11-20 21:48:10 +0100

    Create implementation to Stage1.encode, passed some tests

--- src/main/java/com/facebook/bitEncoderDecoder/Main.java
+++ src/main/java/com/facebook/bitEncoderDecoder/Main.java
@@ -3,6 +3,9 @@ package com.facebook.bitEncoderDecoder;
 public class Main {
 
     public static void main(String[] args) {
+        Stage1 stage = new Stage1();
         System.out.println("Hello, world");
+        stage.encode("abcdefg");
+
     }
 }
