5cfd21c6a835e03bc0dfd5f0e0ff1a09749bdcea
tyczynoprzemyslaw <tyczyno.przemyslaw@gmail.com>
2020-11-21 15:37:53 +0100

    Extend Stage1, all tests passed

--- src/main/java/com/facebook/bitEncoderDecoder/Stage1.java
+++ src/main/java/com/facebook/bitEncoderDecoder/Stage1.java
@@ -4,6 +4,11 @@ public class Stage1 {
 
     public String encode(String input) {
         StringBuilder encodeString = new StringBuilder();
+        if (input == null) {
+            throw new IllegalArgumentException();
+        } else if (input.equalsIgnoreCase(" ")) {
+            return "";
+        }
         for (int i = 0; i < input.length(); i++) {
             encodeString.append(input.charAt(i));
             encodeString.append(input.charAt(i));
