96b2e1c255879fcf0c14c1c7029017afe997b40d
Paweł Dąbrowski <fluorix@gmail.com>
2020-11-22 21:11:03 +0100

    added test for Utils.randomizeChar

--- src/test/java/com/facebook/com/bitEncoderDecoder/UtilsTest.java
+++ src/test/java/com/facebook/com/bitEncoderDecoder/UtilsTest.java
@@ -3,6 +3,7 @@ package com.facebook.com.bitEncoderDecoder;
 import com.facebook.bitEncoderDecoder.Utils;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UtilsTest {
@@ -14,8 +15,24 @@ public class UtilsTest {
         int UPPER = 20;
 
         while(--loops > 0){
-            System.out.println(Utils.getRandomInRange(LOWER, UPPER));
             assertTrue(Utils.getRandomInRange(LOWER, UPPER) <= UPPER);
+        }
+    }
+
+    @Test
+    public void shouldRandomizeCharWork(){
+        // given
+        char[] chars = {'a', 'b', 'V', '.', ' ', '9', 'q', ';'};
+
+        // when
+        char[] actual = new char[chars.length];
+        for (int i = 0; i < chars.length; i++){
+            actual[i] = Utils.randomizeChar(chars[i]);
+        }
+
+        // then
+        for (int i = 0; i < chars.length; i++) {
+            assertNotEquals(chars[i], actual[i]);
         }
     }
 }
