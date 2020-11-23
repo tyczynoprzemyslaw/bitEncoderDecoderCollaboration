7c3a9a35c234d5329189c2ffa21660cfcc5825bd
tyczynoprzemyslaw <tyczyno.przemyslaw@gmail.com>
2020-11-23 15:46:46 +0100

    Created new tests

--- src/test/java/com/facebook/com/bitEncoderDecoder/Stage2Test.java
+++ src/test/java/com/facebook/com/bitEncoderDecoder/Stage2Test.java
@@ -4,6 +4,9 @@ import com.facebook.bitEncoderDecoder.Stage2;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Stage2Test {
@@ -14,13 +17,13 @@ public class Stage2Test {
     public void shouldSendGivenEmptyReturnEmpty() {
         // given
         String input = "";
-        String expected = "";
+        String outputString = "";
 
         // when
         String actual = stage2.send(input);
 
         // then
-        assertEquals(expected, actual);
+        assertEquals(outputString, actual);
     }
 
     @Disabled
@@ -28,26 +31,39 @@ public class Stage2Test {
     public void shouldSendGivenStringReturnStringCoded() {
         // given
         String inputString = "aaabbbccc";
-        String expected = "auatbblcc";
-        int[] result = new int[expected.length() / 3];
-        int[] expectedArray = new int[expected.length() / 3];
+        String outputString = "aactbbxcc";
+        List<String> strings = new ArrayList<>();
+        List<String> outputStringsOf3Chars = new ArrayList<>();
+        int index = 0;
+        boolean result = true;
 
         // when
-        for (int c : expectedArray) {
-            c = 2;
+        while (index < inputString.length()) {
+            strings.add(inputString.substring(index, Math.min(index + 3, inputString.length())));
+            outputStringsOf3Chars.add(outputString.substring(index, Math.min(index + 3, inputString.length())));
+            index += 3;
         }
-        for (int i = 0; i < inputString.length(); i += 3) {
-            for (int j = i; j < inputString.length(); j++) {
-                if (inputString.charAt(j) == expected.charAt(j)) {
-                    result[i]++;
+        for (int i = 0; i < strings.size(); i++) {
+            int checker = 0;
+            if (strings.get(i).equals(outputStringsOf3Chars.get(i))) {
+                result = false;
+                continue;
+            }
+            checker = 0;
+            for (int j = 0; j < 3; j++) {
+                if ((strings.get(i)).charAt(j) == (outputStringsOf3Chars.get(i)).charAt(j)) {
+                    checker++;
+                }
+                if (j == 2 && checker < 2) {
+                    result = false;
                 }
             }
+
         }
 
         // then
-        assertArrayEquals(result, expectedArray);
+        assertTrue(result);
     }
-
 
     @Test
     public void shouldSendGivenNullThrowIllegalArgumentException() {
@@ -69,19 +85,31 @@ public class Stage2Test {
         // then
         assertEquals(input.length(), actual.length());
     }
-/*  //i didn't know we should keep it
+
     @Test
-    public void shouldSendGivenSpaceReturnEmpty() {
+    public void shouldInputLengthBeDivisibleBy3() {
         // given
-        String input = " ";
-        String expected = "";
+        String input = "xuxpiplkk";
 
         // when
-        String actual = stage1.encode(input);
+        boolean result = input.length() % 3 == 0;
 
         // then
-        assertEquals(expected, actual);
+        assertTrue(result);
     }
 
- */
+    @Test
+    public void shouldSendGivenSpaceReturnEmpty() {
+        // given //when
+        String input = " ";
+        String outputString = "";
+
+        // when
+        String actual = stage2.send(input);
+
+        // then
+        assertEquals(outputString, actual);
+    }
+
+
 }
