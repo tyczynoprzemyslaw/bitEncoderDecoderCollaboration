295193ac0e1eb18b8180d9191913f692ad84892e
Paweł Dąbrowski <fluorix@gmail.com>
2020-11-24 09:20:19 +0100

    example parameterized test in Stage3

--- src/main/java/com/facebook/bitEncoderDecoder/Stage3.java
+++ src/main/java/com/facebook/bitEncoderDecoder/Stage3.java
@@ -4,9 +4,6 @@ public class Stage3 {
 
     public String decode(String input){
 
-        for (int i = 0; i < 100; i++){
-            System.out.println(Math.random());
-        }
         return null;
     }
 }
