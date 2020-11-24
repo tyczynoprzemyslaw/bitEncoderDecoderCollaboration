46e4c8285debacb8fb74c739f82f4177959ee00c
Paweł Dąbrowski <fluorix@gmail.com>
2020-11-24 09:11:37 +0100

    Stage3 created class

---
+++ src/main/java/com/facebook/bitEncoderDecoder/Stage3.java
@@ -0,0 +1,12 @@
+package com.facebook.bitEncoderDecoder;
+
+public class Stage3 {
+
+    public String decode(String input){
+
+        for (int i = 0; i < 100; i++){
+            System.out.println(Math.random());
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/facebook/com/bitEncoderDecoder/Stage3Test.java
@@ -0,0 +1,4 @@
+package com.facebook.com.bitEncoderDecoder;
+
+public class Stage3Test {
+}
