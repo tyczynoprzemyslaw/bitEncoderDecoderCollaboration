ce0f990f3122204c4327d9c82777650b637db9cc
Rafal <rpajdak@gmail.com>
2020-11-29 17:47:55 +0100

    Implement doubledChar method.

--- src/main/java/com/facebook/bitEncoderDecoder/Stage3.java
+++ src/main/java/com/facebook/bitEncoderDecoder/Stage3.java
@@ -1,9 +1,25 @@
 package com.facebook.bitEncoderDecoder;
 
+import java.util.*;
+
 public class Stage3 {
 
-    public String decode(String input){
 
-        return null;
+    public  String decode(String input) {
+      return null;
     }
+
+    private  char doubledChar(String trippled) {
+        char doubledChar = ' ';
+        Set<Character> mapWithChars = new HashSet<>();
+        for (int i = 0; i < trippled.length(); i++) {
+            if (mapWithChars.contains(trippled.charAt(i))) {
+                return trippled.charAt(i);
+            } else {
+                mapWithChars.add(trippled.charAt(i));
+            }
+        }
+        return doubledChar;
+    }
+
 }
