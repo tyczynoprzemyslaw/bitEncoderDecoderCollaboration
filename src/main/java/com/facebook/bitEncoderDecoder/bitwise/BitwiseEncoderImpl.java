515e9aacd21a881c71471916395ef37812406179
Rafal <rpajdak@gmail.com>
2020-12-08 17:32:13 +0100

    Implement charHexToBin method.

--- src/main/java/com/facebook/bitEncoderDecoder/bitwise/BitwiseEncoderImpl.java
+++ src/main/java/com/facebook/bitEncoderDecoder/bitwise/BitwiseEncoderImpl.java
@@ -8,4 +8,16 @@ public class BitwiseEncoderImpl implements Encoder {
     public String encode(String input) {
         return null;
     }
+
+
+    private String charHexToBin(Character ch) {
+        String bin = Integer.toBinaryString(ch);
+        StringBuilder result = new StringBuilder();
+        if (bin.length() == 6) {
+            return result.append("00").append(bin).toString();
+        } else if (bin.length() == 8) {
+            return result.append(bin).toString();
+        }
+        return result.append("0").append(bin).toString();
+    }
 }
