43847aaa4ea236b4902b28a0a87eb6296ea254c2
Rafal <rpajdak@gmail.com>
2020-12-16 20:12:43 +0100

    Implement method to convert char to Binary.

--- src/main/java/com/facebook/bitEncoderDecoder/bitwise/BitwiseTransmitterImpl.java
+++ src/main/java/com/facebook/bitEncoderDecoder/bitwise/BitwiseTransmitterImpl.java
@@ -4,6 +4,8 @@ import com.facebook.bitEncoderDecoder.app.Transmitter;
 import com.facebook.bitEncoderDecoder.utils.RandomProvider;
 import com.facebook.bitEncoderDecoder.utils.RandomProviderImpl;
 
+import java.util.Arrays;
+
 public class BitwiseTransmitterImpl implements Transmitter {
 
     private final RandomProvider randomProvider;
@@ -14,6 +16,33 @@ public class BitwiseTransmitterImpl implements Transmitter {
 
     @Override
     public String send(String input) {
+        if (input.isBlank()) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        String[] chars = new String[input.length()];
+
+
         return null;
     }
+
+    private String charHexToBin(Character ch) {
+        String bin = Integer.toBinaryString(ch);
+        StringBuilder result = new StringBuilder();
+        if (bin.length() == 8) {
+            return result.append(bin).toString();
+        } else {
+            int desiredLength = 8;
+            int missingZeros = desiredLength - bin.length();
+            for (int i = 0; i < missingZeros; i++) {
+                result.append("0");
+            }
+        }
+        result.append(bin);
+        return result.toString();
+    }
+
+
 }
