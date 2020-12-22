8f9ad234ed4a5ab2b8d7455762b421c922947381
Rafal Pajdak <rpajdak@gmail.com>
2020-12-22 20:41:29 +0100

    Implement method to get array of binary representation of char.

--- src/main/java/com/facebook/bitEncoderDecoder/bitwise/BitwiseDecoderImpl.java
+++ src/main/java/com/facebook/bitEncoderDecoder/bitwise/BitwiseDecoderImpl.java
@@ -4,8 +4,21 @@ import com.facebook.bitEncoderDecoder.app.Decoder;
 
 public class BitwiseDecoderImpl implements Decoder {
 
+
     @Override
     public String decode(String input) {
+
+        String[] binaryChars = getStrings(input);
         return null;
     }
+
+
+    private String[] getStrings(String input) {
+        String[] chars = new String[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            chars[i] = CharToBinConverter.charHexToBin(input.charAt(i));
+        }
+        return chars;
+    }
+
 }
